ac7dfd721cfe8076ba6ebf059a2cd41adc4afd73
bfoley12 <brendanfoley1214@gmail.com>
2025-01-02 11:06:03 -0500

    inital_commit

---
+++ README.md
@@ -0,0 +1,4 @@
+flow_analysis (temp name) is currently under development.
+
+Implement batch correction and normalisation along with machine learning
+techniques to improve automation of flow cytometry data.
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flow_analysis",
+    version="0.1.0",
+    author="Brendan Foley",
+    author_email="brendanfoley1214@gmail.com",
+    description="A package for flow cytometry data analysis",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bfoley12/flow_analysis",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "polars",
+        "matplotlib",
+        "seaborn"
+    ],
+    include_package_data=True,
+)
